691f4d181605df131aef960568612fc439d8c9b3
unknown <msuresh2468@gmail.com>
2025-02-20 22:59:09 +0530

    Add new files

---
+++ application/views/portal/installed-items.php
@@ -0,0 +1,82 @@
+<?php
+$this->load->view('portal/layout/header');
+if ($this->session->flashdata('status')) : ?>
+    <div class="alert alert-success">
+        <?= $this->session->flashdata('status'); ?>
+    </div>
+<?php endif; ?>
+<div class="portal_section">
+    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow d-print-none">
+        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#"> Welcome <?php echo $this->session->auth_user['username'] ?></a>
+        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> -->
+        <ul class="navbar-nav bg px-3">
+            <li class="nav-item text-nowrap">
+                <a class="nav-link text-warning fw-bold" href="<?php echo base_url()?>logout">Logout <i class="fa fa-sign-out"></i></a>
+            </li>
+        </ul>
+    </header>
+    <div class="container-fluid">
+        <div class="row">
+            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block sidebar collapse d-print-none shadow-lg">
+                <div class="position-sticky pt-4">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="index">
+                                Dashboard
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo base_url()?>portal/purchase-orders">
+                                Purchase Orders
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo base_url()?>portal/supplied-items">
+                                Supplied Items
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo base_url()?>portal/not-supplied-items">
+                                Not Supplied Items
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo base_url()?>portal/installed-items">
+                                Installed Items
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo base_url()?>portal/not-installed-items">
+                                Not Installed Items
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo base_url()?>portal/installed-items-after-cutoff-date">
+                                Installed Items After Cutoff Date
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo base_url()?>portal/installed-items-before-cutoff-date">
+                                Installed Items Before Cutoff Date
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo base_url()?>portal/supply-status">
+                                Supply Status
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                    <h4>Installed Items</h4>
+                </div>
+            </main>
+        </div>
+    </div>
+</div>
+<?php $this->load->view('portal/layout/footer'); ?>
